63095673729d4b9c7067de01c75309a7f5379b32
gusalbukrk <gusalbukrk@gmail.com>
2021-09-12 18:07:35 -0300

    add templates, add `create` script

---
+++ templates/library/src/index.ts
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-extraneous-dependencies, node/no-extraneous-import */
+
+import { reduce } from 'lodash-es';
+
+import add from './add';
+import mul from './mul';
+
+const addArr = (...arr: number[]): number => reduce(arr, add, 0);
+
+const mulArr = (...arr: number[]): number => reduce(arr, mul, 1);
+
+export { addArr as add, mulArr as mul };
+
+/* eslint-enable import/no-extraneous-dependencies, node/no-extraneous-import */
